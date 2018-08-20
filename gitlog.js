c160002fe717629631d51c808906222068b09f2c
Tim Groeneboom <tim@timgroeneboom.com>
2018-08-20 13:59:43 +0200

    added eslint to beautify code

--- gitlog.js
+++ gitlog.js
@@ -1,102 +1,92 @@
 
 module.exports = 
 {
-   getLog : function(url, user, pass, branch, entries, commitID, callback)
-   {
-        var repoPath = 'tmp';
+	getLog : function(url, user, pass, branch, entries, commitID, callback){
+		var repoPath = "tmp";
         
-        var fs = require('fs-extra');
-        var shell = require('shelljs');
+		var fs = require("fs-extra");
+		var shell = require("shelljs");
 
-        if (!fs.existsSync(repoPath))
-        {
-            shell.mkdir('-p', repoPath);
-        }else
-        {
-            fs.removeSync(repoPath);
-            shell.mkdir('-p', repoPath);
-        }
+		if (!fs.existsSync(repoPath)){
+			shell.mkdir("-p", repoPath);
+		}else{
+			fs.removeSync(repoPath);
+			shell.mkdir("-p", repoPath);
+		}
 
-        var git = require('simple-git/promise')(repoPath);
+		var git = require("simple-git/promise")(repoPath);
 
-        const remote = `https://${user}:${pass}@${url}`;
+		const remote = `https://${user}:${pass}@${url}`;
 
-        console.log("Cloning to " + repoPath);
-        git.clone(remote, "../" + repoPath)
-            .then(() =>
-            {
-                console.log("   Finished!");
-                fetch();
-            } ).catch( err => console.log(err) );
+		console.log("Cloning to " + repoPath);
+		git.clone(remote, "../" + repoPath)
+			.then(() =>{
+				console.log("   Finished!");
+				fetch();
+			} ).catch( err => console.log(err) );
 
 
-        function fetch()
-        {
-            console.log("Fetching...");
-            git.fetch()
-                .then(()=>
-                {
-                    console.log("   Finished!");
-                    console.log("Switching to branch " + branch);
-                    git.checkout(branch)
-                        .then(()=>
-                        {
-                            console.log("   Finished!");
-                            pull();
-                        });
-                }).catch( err => console.log(err) );
-        }
+		function fetch(){
+			console.log("Fetching...");
+			git.fetch()
+				.then(()=>{
+					console.log("   Finished!");
+					console.log("Switching to branch " + branch);
+					git.checkout(branch)
+						.then(()=>{
+							console.log("   Finished!");
+							pull();
+						});
+				}).catch( err => console.log(err) );
+		}
 
-	    function pull()
-        {
-            console.log("Pulling...");
-            git.pull()
-                .then(()=>
-                {
-                    console.log("   Finished!");
-                    getLog();
-                }).catch( err => console.log(err) );
-        }
+		function pull(){
+			console.log("Pulling...");
+			git.pull()
+				.then(()=>{
+					console.log("   Finished!");
+					getLog();
+				}).catch( err => console.log(err) );
+		}
 
-        function getLog()
-        {
-            console.log("Getting log...")
-            git.log().then( log =>
-            {
-                var returnLog = [];
+		function getLog(){
+			console.log("Getting log...");
+			git.log().then( log =>{
+				var returnLog = [];
 
-                var index = 0;
-                var found = false;
+				var index = 0;
+				var found = false;
 
-                log.all.forEach(element => 
-                {
-                    if( element.hash == commitID )
-                    {
-                        index = log.all.indexOf(element);
-                        found = true;
-                    }
-                });
+				log.all.forEach(element => {
+					if( element.hash == commitID ){
+						index = log.all.indexOf(element);
+						found = true;
+					}
+				});
 
-                var i = index;
-                var end = index+entries
-                if( end > log.all.length)
-                    end = log.all.length;
-    
-                for(i; i < end; i++)
-                {
-                    var element = log.all[i];
-    
-                    returnLog.push({
-                        "hash" : element.hash,
-                        "date" : element.date,
-                        "message" : element.message
-                    });
-                }
+				if( found ){
+					var i = index;
+					var end = index+entries;
+					if( end > log.all.length)
+						end = log.all.length;
+        
+					for(i; i < end; i++){
+						var element = log.all[i];
+        
+						returnLog.push({
+							"hash" : element.hash,
+							"date" : element.date,
+							"message" : element.message
+						});
+					}
+				}else{
+					console.log("commit id not found in repository");
+				}
 
-                console.log("   Finished!");
+				console.log("   Finished!");
 
-                callback(returnLog);
-            }).catch(err => console.log(err));
-        }
-   }
+				callback(returnLog);
+			}).catch(err => console.log(err));
+		}
+	}
 };
--- index.js
+++ index.js
@@ -1,261 +1,243 @@
 // Initialise .env config.
-require('dotenv').config();
+require("dotenv").config();
 
 // Options
 var options = {
-  port: process.env.PORT || 80, // Heroku port or 80.
-  unityAPIBase: "https://build-api.cloud.unity3d.com", // URI (e.g. href) recieved in web hook payload.
-  unityCloudAPIKey: process.env.UNITYCLOUD_KEY,
-  hockeyappAPIUpload: "https://rink.hockeyapp.net/api/2/apps/upload",
-  hockeyappAPIKey: process.env.HOCKEYAPP_KEY,
-  repoURL : process.env.GIT_URL,
-  repoUser : process.env.GIT_USER,
-  repoPass : process.env.GIT_PASS,
-  repoBranch : process.env.GIT_BRANCH,
-  entries : process.env.GIT_ENTRIES
+	port: process.env.PORT || 80, // Heroku port or 80.
+	unityAPIBase: "https://build-api.cloud.unity3d.com", // URI (e.g. href) recieved in web hook payload.
+	unityCloudAPIKey: process.env.UNITYCLOUD_KEY,
+	hockeyappAPIUpload: "https://rink.hockeyapp.net/api/2/apps/upload",
+	hockeyappAPIKey: process.env.HOCKEYAPP_KEY,
+	repoURL : process.env.GIT_URL,
+	repoUser : process.env.GIT_USER,
+	repoPass : process.env.GIT_PASS,
+	repoBranch : process.env.GIT_BRANCH,
+	entries : process.env.GIT_ENTRIES
 };
 
 // Imports
-var path = require('path'),
-    fs = require('fs'),
-    express = require('express'),
-    app = express(),
-    http = require('http'),
-    https = require('https'),
-    server = http.Server(app),
-    bodyParser = require('body-parser'),
-    najax = require('najax'),
-    FormData = require('form-data'),
-    _ = require('lodash'),
-    url = require("url"),
-    path = require("path"),
-    gitlog = require("./gitlog");
+var path = require("path"),
+	fs = require("fs"),
+	express = require("express"),
+	app = express(),
+	http = require("http"),
+	https = require("https"),
+	server = http.Server(app),
+	bodyParser = require("body-parser"),
+	najax = require("najax"),
+	FormData = require("form-data"),
+	url = require("url"),
+	gitlog = require("./gitlog");
 
 // Run Server
-var server = server.listen( options.port, function(){
-  console.log('listening on *:' + options.port );
+server.listen( options.port, function(){
+	console.log("listening on *:" + options.port );
 });
 
 // Configure Express
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 // parse application/json
 // app.use(bodyParser.json()); // Parse all
 var jsonParser = bodyParser.json();
 
-app.get('/', function(req, res){
-  res.sendFile( __dirname + '/index.html' );
+app.get("/", function(req, res){
+	res.sendFile( __dirname + "/index.html" );
 });
 
 // POST /api/users gets JSON bodies 
-var mainRes;
-app.post('/build', jsonParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
+app.post("/build", jsonParser, function (req, res) {
+	if (!req.body) return res.sendStatus(400);
+    
+	// 1. Get Build API URL
+	var buildAPIURL = req.body.links.api_self.href;
+	if( !buildAPIURL ) {
+		// URL not available.
+		res.setHeader("Content-Type", "application/json");
+		res.send({
+			error: true,
+			message: "No build link from Unity Cloud Build webhook"
+		});
+	} else {
+		// URL not available.
+		res.setHeader("Content-Type", "application/json");
+		res.send({
+			error: false,
+			message: "Process begun for project '" + req.body.projectName + "' platform '" + req.body.buildTargetName + "'."
+		});
+	}
 
-    mainRes = res;
-
-    // 1. Get Build API URL
-    var buildAPIURL = req.body.links.api_self.href;
-    if( !buildAPIURL ) {
-        // URL not available.
-        res.setHeader('Content-Type', 'application/json');
-        res.send({
-            error: true,
-            message: "No build link from Unity Cloud Build webhook"
-        });
-    } else {
-        // URL not available.
-        res.setHeader('Content-Type', 'application/json');
-        res.send({
-            error: false,
-            message: "Process begun for project '" + req.body.projectName + "' platform '" + req.body.buildTargetName + "'."
-        });
-    }
-
-    // 2. Grab binary URL from Unity Cloud API
-    getBuildDetails( buildAPIURL );
+	// 2. Grab binary URL from Unity Cloud API
+	getBuildDetails( buildAPIURL );
 });
 
 function getBuildDetails( buildAPIURL ){
-    console.log("1. getBuildDetails: start");
+	console.log("1. getBuildDetails: start");
 
-    najax({ 
-        url: options.unityAPIBase + buildAPIURL,
-        type: 'GET', 
-        headers: {
-            'Authorization': 'Basic ' + options.unityCloudAPIKey
-        },
-        success: function(data){
+	najax({ 
+		url: options.unityAPIBase + buildAPIURL,
+		type: "GET", 
+		headers: {
+			"Authorization": "Basic " + options.unityCloudAPIKey
+		},
+		success: function(data){
 
-            var data = JSON.parse(data);
-            // console.log( data.links.download_primary.href );
+			var parsedData = JSON.parse(data);
+			// console.log( data.links.download_primary.href );
 
-            console.log("1. getBuildDetails: finished");
+			console.log("1. getBuildDetails: finished");
 
-            // 3. Download binary.
-            downloadBinary( data );
+			// 3. Download binary.
+			downloadBinary( parsedData );
 
-        },
-        error: function(error){
-            console.log(error);
-        }
-    });
+		},
+		error: function(error){
+			console.log(error);
+		}
+	});
 }
 
 function downloadBinary( data ){
     
-    var parsed = url.parse( data.links.download_primary.href );
-    var filename = path.basename( parsed.pathname );
-    var binaryURL = data.links.download_primary.href;
+	var parsed = url.parse( data.links.download_primary.href );
+	var filename = path.basename( parsed.pathname );
+	var binaryURL = data.links.download_primary.href;
 
-    console.log("2. downloadBinary: start");
-    console.log("   " + binaryURL);
-    console.log("   " + filename);
+	console.log("2. downloadBinary: start");
+	console.log("   " + binaryURL);
+	console.log("   " + filename);
 
-    deleteFile( filename );
+	deleteFile( filename );
     
-    https.get( binaryURL, (res) => {
-        // console.log('statusCode: ', res.statusCode);
-        // console.log('headers: ', res.headers);
+	https.get( binaryURL, (res) => {
+		// console.log('statusCode: ', res.statusCode);
+		// console.log('headers: ', res.headers);
 
-        var writeStream = fs.createWriteStream(filename, {'flags': 'a'});
+		var writeStream = fs.createWriteStream(filename, {"flags": "a"});
 
-        var len = parseInt(res.headers['content-length'], 10);
-        var cur = 0;
-        var total = len / 1048576; //1048576 - bytes in  1Megabyte
+		var len = parseInt(res.headers["content-length"], 10);
+		var cur = 0;
+		var total = len / 1048576; //1048576 - bytes in  1Megabyte
 
-        res.on('data', (chunk) => {
+		res.on("data", (chunk) => {
         
-            cur += chunk.length;
-            writeStream.write(chunk, 'binary');
+			cur += chunk.length;
+			writeStream.write(chunk, "binary");
 
-            console.log("Downloading " + (100.0 * cur / len).toFixed(2) + "%, Downloaded: " + (cur / 1048576).toFixed(2) + " mb, Total: " + total.toFixed(2) + " mb");
-        });
+			console.log("Downloading " + (100.0 * cur / len).toFixed(2) + "%, Downloaded: " + (cur / 1048576).toFixed(2) + " mb, Total: " + total.toFixed(2) + " mb");
+		});
 
-        res.on('end', () => {
+		res.on("end", () => {
 
-            console.log("2. downloadBinary: finished");
-            writeStream.end();
+			console.log("2. downloadBinary: finished");
+			writeStream.end();
 
-        });
+		});
 
-        writeStream.on('finish', () => {
+		writeStream.on("finish", () => {
 
-            // console.log("2. downloadBinary: file finished");          
-            uploadToHockeyApp( data, filename );
-        });
+			// console.log("2. downloadBinary: file finished");          
+			uploadToHockeyApp( data, filename );
+		});
 
-    }).on('error', (e) => {
-      console.error(e);
-    });
+	}).on("error", (e) => {
+		console.error(e);
+	});
 }
 
 function uploadToHockeyApp( data, filename )
 {
-    console.log("3. get gitlog");
+	console.log("3. get gitlog");
 
-    gitlog.getLog( 
-        options.repoURL, 
-        options.repoUser, 
-        options.repoPass, 
-        options.repoBranch,
-        options.entries,
-        data.lastBuiltRevision,
-        function(log)
-        {
-            console.log("4. uploadToHockeyApp: start");
-            // console.log("readfile: " + filename);
+	gitlog.getLog( 
+		options.repoURL, 
+		options.repoUser, 
+		options.repoPass, 
+		options.repoBranch,
+		options.entries,
+		data.lastBuiltRevision,
+		function(log)
+		{
+			console.log("4. uploadToHockeyApp: start");
+			// console.log("readfile: " + filename);
         
-            var readable = fs.createReadStream( filename );
-            readable.on('error', () => {
-                console.log('Error reading binary file for upload to HockeyApp');
-            });
+			var readable = fs.createReadStream( filename );
+			readable.on("error", () => {
+				console.log("Error reading binary file for upload to HockeyApp");
+			});
         
-            // HockeyApp properties
-            var HOCKEY_APP_HOST = 'rink.hockeyapp.net';
-            var HOCKEY_APP_PATH = '/api/2/apps/upload/';
-            var HOCKEY_APP_PROTOCOL = 'https:';
+			// HockeyApp properties
+			var HOCKEY_APP_HOST = "rink.hockeyapp.net";
+			var HOCKEY_APP_PATH = "/api/2/apps/upload/";
+			var HOCKEY_APP_PROTOCOL = "https:";
         
-            var notes = "Automated release triggered from Unity Cloud Build.\n"
+			var notes = "Automated release triggered from Unity Cloud Build.\n"
                 + "Commit ID: " + data.lastBuiltRevision + "\n"
                 + "Build Target Name: " + data.buildTargetName;
 
-            if( notes.length > 0 )
-            {
-                notes += "\n\n" + "<b>Recent Changes:</b>" + "\n";
-                for(var i = 0; i < log.length; i++)
-                {
-                    notes += "\n" + log[i].date + "\n" + log[i].message + "\n";
-                }
-            }
+			if( notes.length > 0 )
+			{
+				notes += "\n\n" + "<b>Recent Changes:</b>" + "\n";
+				for(var i = 0; i < log.length; i++)
+				{
+					notes += "\n" + log[i].date + "\n" + log[i].message + "\n";
+				}
+			}
 
-            // Create FormData
-            var form = new FormData();
-            form.append('status', 2);
-            // form.append('mandatory', MANDATORY_TYPE[options.mandatory]);
-            form.append('notes', notes);
-            form.append('notes_type', 0);
-            form.append('notify', 0);
-            form.append('ipa', readable);
+			// Create FormData
+			var form = new FormData();
+			form.append("status", 2);
+			// form.append('mandatory', MANDATORY_TYPE[options.mandatory]);
+			form.append("notes", notes);
+			form.append("notes_type", 0);
+			form.append("notify", 0);
+			form.append("ipa", readable);
         
-            var req = form.submit({
-            host: HOCKEY_APP_HOST,
-            path: HOCKEY_APP_PATH,
-            protocol: HOCKEY_APP_PROTOCOL,
-            headers: {
-                'Accept': 'application/json',
-                'X-HockeyAppToken': options.hockeyappAPIKey
-            }
-            }, function (err, res) {
-                if (err) {
-                    console.log(err);
-                }
+			var req = form.submit({
+				host: HOCKEY_APP_HOST,
+				path: HOCKEY_APP_PATH,
+				protocol: HOCKEY_APP_PROTOCOL,
+				headers: {
+					"Accept": "application/json",
+					"X-HockeyAppToken": options.hockeyappAPIKey
+				}
+			}, function (err, res) {
+				if (err) {
+					console.log(err);
+				}
         
-                if (res.statusCode !== 200 && res.statusCode !== 201) {
-                    console.log('Uploading failed with status ' + res.statusCode);
-                    console.log(res);
-                    // res.on('data', function (chunk) {
-                    //   console.log(chunk);
-                    //             // res.on('end', function () {
-                    //   console.log("end");
-                    // });
-                    return;
-                }
-        
-                var jsonString = '';
-                res.on('data', (chunk) => {
-                
-                    jsonString += String.fromCharCode.apply(null, new Uint16Array( chunk ));
-        
-                });
-        
-                res.on('end', () => {
-        
-                    console.log("5. uploadToHockeyApp: finished");
+				if (res.statusCode !== 200 && res.statusCode !== 201) {
+					console.log("Uploading failed with status " + res.statusCode);
+					console.log(res);
+					return;
+				}
 
-                    deleteFile( filename );
-                });
-            });
+				res.on("end", () => {
+					console.log("5. uploadToHockeyApp: finished");
+
+					deleteFile( filename );
+				});
+			});
             
-            // Track upload progress.
-            // console.log( req );
-            var len = parseInt( req.getHeader( 'content-length' ), 10);
-            var cur = 0;
-            var total = len / 1048576; //1048576 - bytes in  1Megabyte
+			// Track upload progress.
+			// console.log( req );
+			var len = parseInt( req.getHeader( "content-length" ), 10);
+			var cur = 0;
+			var total = len / 1048576; //1048576 - bytes in  1Megabyte
         
-            req.on('data', (chunk) => {
-                cur += chunk.length;
-                console.log("Downloading " + (100.0 * cur / len).toFixed(2) + "%, Downloaded: " + (cur / 1048576).toFixed(2) + " mb, Total: " + total.toFixed(2) + " mb");
-            });    
-        });
+			req.on("data", (chunk) => {
+				cur += chunk.length;
+				console.log("Downloading " + (100.0 * cur / len).toFixed(2) + "%, Downloaded: " + (cur / 1048576).toFixed(2) + " mb, Total: " + total.toFixed(2) + " mb");
+			});    
+		});
 }
 
 // Delete file, used to clear up any binary downloaded.
 function deleteFile( filename ){
-    fs.exists(filename, function(exists) { 
-      if (exists) { 
-        // Delete File.
-        fs.unlink( filename );
-      } 
-    }); 
+	fs.exists(filename, function(exists) { 
+		if (exists) { 
+			// Delete File.
+			fs.unlink( filename );
+		} 
+	}); 
 }
